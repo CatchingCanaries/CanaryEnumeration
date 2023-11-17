c90bc14351d991a5f20b58719051ef0092603a5a
CatchingCanaries <149830753+CatchingCanaries@users.noreply.github.com>
2023-11-17 07:53:23 -0800

    Update LSASS_Fake_Account_Canary_Detection.ps1

--- User-Access Canary Detections/LSASS_Fake_Account_Canary_Detection.ps1
+++ User-Access Canary Detections/LSASS_Fake_Account_Canary_Detection.ps1
@@ -10,11 +10,11 @@ foreach ($event in $events) {
         Write-Host 'Step 2 - Logon Type 9 Identified' -ForegroundColor Yellow
 
         if (($event.ReplacementStrings[22] -ne $event.ReplacementStrings[1]) -or ($event.ReplacementStrings[22] -ne "-")) {
-			Write-Host 'Step 3 - Subject vs New Logon Account Name Match' -ForegroundColor Yellow
-            Write-Host 'Step 4 - Mismatched Account Name vs Network Account Name' -ForegroundColor Yellow
-            Write-Host "`n$($event.ReplacementStrings[1]) " -ForegroundColor Green -NoNewLine 
-			Write-Host "created deception user " -NoNewLine
-			Write-Host "$($event.ReplacementStrings[23])\$($event.ReplacementStrings[22])" -ForegroundColor Red
+		Write-Host 'Step 3 - Subject vs New Logon Account Name Match' -ForegroundColor Yellow
+            	Write-Host 'Step 4 - Mismatched Account Name vs Network Account Name' -ForegroundColor Yellow
+            	Write-Host "`n$($event.ReplacementStrings[1]) " -ForegroundColor Green -NoNewLine 
+		Write-Host "created deception user " -NoNewLine
+		Write-Host "$($event.ReplacementStrings[23])\$($event.ReplacementStrings[22])" -ForegroundColor Red
             Write-Host ''
         }
     }
